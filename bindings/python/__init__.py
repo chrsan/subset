02d7637017f9cc3e915104812a53c6014d486ced
Christer Sandberg <chrsan@gmail.com>
2024-04-27 11:46:38 +0200

    Re-exports doesn't work as in Rust

--- bindings/python/__init__.py
+++ bindings/python/__init__.py
@@ -1,6 +1,16 @@
 # ruff: noqa: F401 F821
 from .lib import Font  # type: ignore
-from .types import FontStyle, Glyph, Path, PathVerb, Point, Transform  # type: ignore
+from .types import FontStyle, Glyph, Path, PathVerb, Point, Transform
 
 del lib
 del types
+
+__all__ = [
+    "Font",
+    "FontStyle",
+    "Glyph",
+    "Path",
+    "PathVerb",
+    "Point",
+    "Transform",
+]
